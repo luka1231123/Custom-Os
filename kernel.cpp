18d7ff1022d12a1762bbbf75f0242ee657991307
luka1231123 <52918050+luka1231123@users.noreply.github.com>
2023-07-19 15:22:01 +0400

    Add files via upload

---
+++ kernel.cpp
@@ -0,0 +1,20 @@
+#include "gdt.h"
+
+
+
+void printf(char* str){
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	
+	for(int i=0;str[i]!='\0'; ++i){
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; 
+	}
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+	printf("zup");
+	
+	GlobalDescriptorTable gdt;
+	
+	while(1);
+}
